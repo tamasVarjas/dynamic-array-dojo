5087c187f7c879257e45d38c5d1d8aa76df45767
Tamás Varjas <tamas.varjas00@gmail.com>
2018-03-21 10:42:33 +0100

    Implemented 'add' and 'remove' methods and also 'toString' override

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,41 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
 public class DynamicIntArray {
 
+    private int[] array;
+    private int lastIndex;
+
+    public DynamicIntArray() {
+        array = new int[10];
+        lastIndex = -1;
+    }
+
+    public DynamicIntArray(int elementCount) {
+        array = new int[elementCount];
+        lastIndex = -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(array.length * 2);
+        for (int i = 0; i <= lastIndex; i++ ) {
+            sb.append(" ");
+            sb.append(array[i]);
+        }
+        return sb.toString();
+    }
+
+    public void add(int num) {
+        if (lastIndex == array.length - 1) {
+            int[] tempArray = array;
+            array = new int[array.length * 2];
+            System.arraycopy(tempArray, 0, array, 0, tempArray.length);
+        }
+        array[++lastIndex] = num;
+    }
+
+    public void remove(int indexToRemove) {
+        System.arraycopy(array, indexToRemove + 1, array, indexToRemove, lastIndex - indexToRemove);
+        lastIndex--;
+    }
 }
